a748a519b216b2dd60cfa443850cc497758a9cfb
father-gregor <benlinus92@gmail.com>
2018-05-05 21:08:24 +0300

    Add vue-awesome

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,32 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+	<div id="app">
+		<img src="./assets/logo.png">
+		<icon label="No Photos">
+			<icon name="archive"></icon>
+		</icon>
+		<router-view/>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+	import 'vue-awesome/icons/archive'
+	import Icon from 'vue-awesome/components/Icon'
+
+	export default {
+		name: 'App',
+		components: {
+			Icon
+		}
+	}
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
